8a2cf7c4e2bac5f0388088dd411510bb60e90ef1
luckypinata <91746808+luckypinata@users.noreply.github.com>
2022-07-14 21:46:40 +0200

    fixed banks() function for pagination

--- Untitled.R
+++ Untitled.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 require(rvest)
 require(dplyr)
 require(tidyverse)
@@ -9,7 +11,7 @@ unique_states <- unique(states$Country)
 
 banks <- function(states = unique_states) { # function to get all banks for each state
   
-  banks_tibble <- list()
+  banks_tibble <<- list()
   
   for (i in 1:length(states)) {
     
@@ -19,12 +21,36 @@ banks <- function(states = unique_states) { # function to get all banks for each
     
     html <- read_html(url)
     
-    table <- html_table(html)[[1]] %>% 
+    table_1 <- html_table(html)[[1]] %>% 
       as_tibble()
     
-    table$country <- rep(states[i], nrow(table))
+    j <- 2
+
+    while (str_detect(as.character(html), "Next") == TRUE) {
+      
+      url <- as.character(paste0("https://www.swiftbic.com/banks-in-", toupper(states[i]), "-", as.character(j), ".html"))
+      
+      print(url)
+      
+      html <- read_html(url)
+      
+      print(html)
+      
+      table_2 <<- html_table(html)[[1]] %>% as_tibble()
+      
+      table_1 <- rbind(table_1, table_2)
+      
+      print(class(table_1))
+      
+      print(nrow(table_1))
+      
+      j <- j + 1
+      
+    }
     
-    banks_tibble[[i]] <- table
+    table_1$country <- rep(states[i], nrow(table_1))
+    
+    banks_tibble[[i]] <- table_1
     
   }
   
@@ -41,31 +67,58 @@ write.csv(banks_tibble, "data/banks.csv")
 # google maps function
 # load("gmaps.rda")
 banks <- read.csv("data/banks.csv")
-load("gmaps.rda")
+key <- load("gmaps.rda")
 
-branches <- function(banks) {#function}
-
+branches <- function(banks) {
   
+  # get region top level domains
+  region_url <- "https://en.wikipedia.org/wiki/Country_code_top-level_domain"
+  region_domains <- read_html(region_url) %>% 
+    html_nodes(xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/table[1]") %>% 
+    html_table() %>% 
+    .[[1]] %>% 
+    select(1,2) %>%
+    filter(Entity %in% unique_states)
   
+  region_domains$`Name[7]` <- sub("(\\.)(\\w+)", "\\2", region_domains$`Name[7]`)
+  region_domains$`Name[7]` <- toupper(region_domains$`Name[7]`)
   
-"""
-url_1 <- "https://www.swiftbic.com/banks-in-MEXICO.html"
-html_1 <- read_html(url_1)
-table_1 <- html_table(html_1)[[1]] %>%
-  data_frame()
-
-url_2 <- "https://en.wikipedia.org/wiki/List_of_banks_in_Mexico"
-html_2 <- read_html(url_2)
-nodes_2 <- html_nodes(html_2, xpath = "/html/body/div[3]/div[3]/div[5]/div[1]/ul[3]")
-content_2 <- html_text(nodes_2) %>% str_split("\\n") %>% unlist()
-
-for (i in 1:length(content_2)) {
-  if (content_2[i] %like% table[[1]][i]) {
-    print(TRUE)
-  } else {
-    print(FALSE)
+  # https://maps.googleapis.com/maps/api/place/textsearch/json?query=[]&region=[]
+  # need query=
+  # need region=
+  url <- "https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s"
+  
+  for (i in 1:nrow(region_domains)) {
+    
+    # function
+    
   }
+  
 }
-"""
+
+# function draft
+for (i in 1:nrow(region_domains)) {
+  
+  nth_region <- region_domains[i,]
+  print(paste0("nth region:", nth_region))
+  nth_country <- nth_region$Entity
+  print(paste0("nth_country", nth_country))
+  nth_banks <- banks %>% filter(nth_country == country)
+  
+  print(head(nth_banks))
+  
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
