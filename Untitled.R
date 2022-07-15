5babd5da15cd375e1b87878ca52655f7270461b6
luckypinata <91746808+luckypinata@users.noreply.github.com>
2022-07-15 15:48:55 +0200

    added banks_con() function

--- Untitled.R
+++ Untitled.R
@@ -5,11 +5,58 @@ require(dplyr)
 require(tidyverse)
 require(data.table)
 require(tibble)
+require(jsonlite)
 
-states <- read.csv("data/index.csv", sep = ";") # 2017 - 2021
-unique_states <- unique(states$Country)
+# load index data
+states_df <- read.csv("data/index.csv", sep = ";") # 2017 - 2021
+unique_states <- unique(states_df$Country)
 
-banks <- function(states = unique_states) { # function to get all banks for each state
+# function to scrape 5-bank asset concentration from World Bank API
+bank_asset_concentration_scraper <- function(indicator, timeframe) { 
+  
+  # formatting inputs and declaring vars
+  banks_df <- list()
+  api_endpoint <- "https://api.worldbank.org/v2/country/all/"
+  t <- paste0("?date=", timeframe)
+  i <- paste0("indicator/", indicator)
+  
+  # formatting url for api call
+  url <- paste0(api_endpoint, i, t, "&format=json")
+  
+  print(url)
+  
+  # placing GET request and extracting df and max pages
+  r <- fromJSON(url)
+  
+  banks_df[[1]] <- r[[2]]
+  
+  n_pages <- r[[1]][2]
+  
+  #print(nrow(banks_df[[1]]))
+  
+  for (j in 2:as.integer(n_pages)) {
+    
+    url <- paste0(api_endpoint, i, t, "&format=json&page=", j)
+    
+    print(url)
+    
+    r <- fromJSON(url)
+    
+    banks_df[[j]] <- r[[2]]
+    
+    #print(nrow(banks_df[[j]]))
+    
+  }
+  
+  # concatenate df's and write to file
+  f <- bind_rows(banks_df) %>%
+    as_tibble() %>%
+    write.csv("data/banks_con.csv")
+  
+}
+
+# function to get all banks for each state
+banks <- function(states = unique_states) { 
   
   banks_tibble <<- list()
   
@@ -53,19 +100,16 @@ banks <- function(states = unique_states) { # function to get all banks for each
   banks_tibble <<- do.call("rbind", banks_tibble) %>%
     as_tibble()
   
-  # perform some wrangling? will the names translate into accurate maps searches?
-  
 }
 
 write.csv(banks_tibble, "data/banks.csv")
 
 # function to find number of branches for each state
-# google maps function
-# load("gmaps.rda")
+# google maps API function
 banks <- read.csv("data/banks.csv")
 key <- load("gmaps.rda")
 
-branches <- function(banks) {
+n_branches <- function(banks) {
   
   # get region top level domains
   region_url <- "https://en.wikipedia.org/wiki/Country_code_top-level_domain"
@@ -86,7 +130,7 @@ branches <- function(banks) {
   
   for (i in 1:nrow(region_domains)) {
     
-    # function
+    # function (see below)
     
   }
   
